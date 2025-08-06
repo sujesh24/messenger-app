3b82fbd80f6395b2c47e5f89ed6416c016c58b28
sujesh <birwazsujesh@gmail.com>
2025-08-06 15:35:14 +0530

    sgin in scree added

--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -2,23 +2,29 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:messagener_app/core/common/custom_button.dart';
 import 'package:messagener_app/core/common/custom_text_Field.dart';
-import 'package:messagener_app/presentation/screens/auth/signup_screen.dart';
+import 'package:messagener_app/presentation/screens/auth/login_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
+    nameController.dispose();
+    usernameController.dispose();
+    phoneController.dispose();
     super.dispose();
   }
 
@@ -35,45 +41,68 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [
                 SizedBox(height: 30),
                 Text(
-                  "Welcome Back",
+                  "Create Account",
                   style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 SizedBox(height: 10),
                 Text(
-                  "Sign in to continue",
+                  "Please fill in the details to create your account",
                   style: Theme.of(
                     context,
                   ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
                 ),
                 SizedBox(height: 30),
                 CustomTextField(
+                  controller: nameController,
+                  hintText: 'Full Name',
+                  prefixIcon: Icon(Icons.person_outline),
+                ),
+                SizedBox(height: 16),
+                CustomTextField(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  prefixIcon: Icon(Icons.alternate_email_outlined),
+                ),
+                SizedBox(height: 16),
+                CustomTextField(
                   controller: emailController,
-                  hintText: "Email",
+                  hintText: 'Email',
                   prefixIcon: Icon(Icons.email_outlined),
                 ),
                 SizedBox(height: 16),
                 CustomTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  obscureText: true,
-                  suffixIcon: Icon(Icons.visibility_outlined),
-                  prefixIcon: Icon(Icons.lock_outline),
+                  controller: phoneController,
+                  hintText: 'Phone Number',
+                  prefixIcon: Icon(Icons.phone_outlined),
                 ),
                 SizedBox(height: 16),
-                CustomButton(text: 'Login', onPressed: () {}),
+                CustomTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                  prefixIcon: Icon(Icons.lock_outline),
+                  suffixIcon: Icon(Icons.visibility_outlined),
+                ),
+                SizedBox(height: 30),
+                CustomButton(
+                  text: 'Sign Up',
+                  onPressed: () {
+                    // Handle sign up logic
+                  },
+                ),
                 SizedBox(height: 20),
                 Center(
                   child: RichText(
                     text: TextSpan(
-                      text: "Don't have an account? ",
+                      text: "Already have an account? ",
                       style: Theme.of(
                         context,
                       ).textTheme.bodyMedium?.copyWith(color: Colors.grey),
                       children: [
                         TextSpan(
-                          text: "Sign Up",
+                          text: "Login",
                           style: Theme.of(context).textTheme.bodyMedium
                               ?.copyWith(
                                 color: Theme.of(context).colorScheme.primary,
@@ -81,12 +110,8 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                           recognizer: TapGestureRecognizer()
                             ..onTap = () {
-                              // Navigate to Sign Up screen
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => SignupScreen(),
-                                ),
-                              );
+                              // Navigate to Sign in screen
+                              Navigator.of(context).pop();
                             },
                         ),
                       ],
